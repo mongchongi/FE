41c67ab8d353c9273017e49f5886ee932c156af9
bak yeong uk <yeonguk0517@gmail.com>
2025-01-06 16:06:54 +0900

    init project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  );
+};
+
+export default App;
