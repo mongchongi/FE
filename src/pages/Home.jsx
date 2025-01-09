7dd76206b8f1d9cd9f7e3fe349edd2d40a6c8349
bak yeong uk <yeonguk0517@gmail.com>
2025-01-09 13:46:39 +0900

    design : footer UI 제거

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import Banner from '../components/Banner';
 import PageLink from '../components/PageLink';
-import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <>
       <Banner />
       <PageLink />
-      <Footer />
     </>
   );
 };
